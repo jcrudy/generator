75e723b0a43d52433c0d46a1fb38e0acc92c3859
Jason Rudy <jcrudy@gmail.com>
2013-12-18 15:48:42 -0800

    Simplified the test case

--- src/test/scala/testGenerator.scala
+++ src/test/scala/testGenerator.scala
@@ -3,15 +3,11 @@ import org.scalatest._
 
 class GeneratorTest extends FlatSpec {
     "A generator" should "work like so" in {
-        def example = generator[String] { 
-            yld => yld( "first" )
- 
-            for( i <- suspendable(List(1,2,3)); j <- suspendable(List(4,5,6))) { 
-                yld((i*j).toString) 
+        def example = generator[Int] { yld => 
+            for( i <- suspendable(List(1,2,3))) { 
+                yld(i) 
             }
-        
-            yld("last")
         }
-        assert(example.toArray === Array("first","4","5","6","8","10","12","12","15","18","last"))
+        assert(example.toArray === Array(1,2,3))
     }
 }
