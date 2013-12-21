980454b8aded5150901a8cc8e2c03e2be4bd8374
Jason Rudy <jcrudy@gmail.com>
2013-12-20 23:47:51 -0800

    Completely new implementation with more tests

--- src/main/scala/generator.scala
+++ src/main/scala/generator.scala
@@ -1,48 +1,60 @@
 package generator
-import scala.util.continuations._
- 
-class Generator[A] extends Iterator[A] with (A => Unit @ suspendable) {
-    private var a: Option[A] = None
-    private var k: Option[Unit => Unit] = None
-    
-    def next = {
-		val a0 = a.get
-		val k0 = k.get
-	    a = None
-		k = None
-		k0()
-		a0
-    }
-    
-    def hasNext = k.isDefined
-    
-    def apply(a0: A): Unit @ suspendable = {
-		a = Some(a0)
-		shift { 
-			k0: (Unit => Unit) => k = Some(k0)
-		}
-    }
-}
- 
-object Generator {
- 
-    def generator[A]( f: (A => Unit @ suspendable) => Unit @ suspendable): Iterator[A] = {
-        val g = new Generator[A]
-        reset { f(g) }
-        g
-    } 
-    
-    trait SuspendableForeach[A]{ 
-    	def foreach( f: A => Unit @suspendable ): Unit @ suspendable 
-    }
+import scala.util.continuations.{shift, reset, cpsParam}
 
-    def suspendable[A]( ible: Iterable[A]) = new SuspendableForeach[A] {
-        def foreach( f: A => Unit @ suspendable ) : Unit @ suspendable = {
-            val i = ible.iterator
-            while( i.hasNext ) {
-            	f(i.next)
-            }
-        }
-    }
+/** Abstract base class for Python-like generators.
+  *
+  * Classes and objects extending Generator should implement
+  * the generate method, which should call produce.  For example,
+  * the following would create a counter that increments each time the 
+  * next method is called:
+  * 
+  *     object Counter extends Generator[Int] {
+  *         def generate() = {
+  *             var i = 1
+  *             while(true) {
+  *                 produce(i)
+  *                 i += 1
+  *             }
+  *         }
+  *     }
+  */
+abstract class Generator[T] extends Iterator[T] {
+
+	private var result: T = _
+	private var empty = false
+	private var callback: Unit=>Unit = null
+
+	reset {
+		generate()
+		empty = true
+	}
+
+	/** Implementation of this method defines the behavior of the generator.
+	  *
+	  */
+	protected def generate(): Unit @cpsParam[Unit,Unit]
+
+	/** The produce method should be called only within the implementation
+	  * of generate.  The resulting Generator returns the arguments of 
+	  * produce as they are called in generate each time the next method is 
+	  * called.
+	  *
+	  */
+	protected def produce(item: T) = {
+		shift {f: (Unit=>Unit) =>
+			callback = f
+			result = item
+		}
+	}
+
+	def hasNext: Boolean = !empty
+
+	def next(): T = {
+		if (empty) {
+			throw new NoSuchElementException("reached generator end")
+		}
+		val thisResult = result
+		callback()
+		thisResult
+	}
 }
- 
--- src/test/scala/testGenerator.scala
+++ src/test/scala/testGenerator.scala
@@ -1,13 +1,95 @@
-import generator.Generator.{generator, suspendable}
 import org.scalatest._
+import generator._
 
 class GeneratorTest extends FlatSpec {
-    "A generator" should "work like so" in {
-        def example = generator[Int] { yld => 
-            for( i <- suspendable(List(1,2,3))) { 
-                yld(i) 
+    "A Generator" should "be able to produce a Counter object" in {
+        object Counter extends Generator[Int] {
+            def generate() = {
+                var i = 1
+                while(true) {
+                    produce(i)
+                    i += 1
+                }
             }
         }
-        assert(example.toArray === Array(1,2,3))
+
+        val counts = for (i <- 0 until 10) yield Counter.next()
+        assert(counts === Vector(1,2,3,4,5,6,7,8,9,10))
+    }
+
+    it should "be able to act as a base class for a Counter class" in {
+        class Counter extends Generator[Int] {
+            def generate() = {
+                var i = 1
+                while(true) {
+                    produce(i)
+                    i += 1
+                }
+            }
+        }
+
+        // Instantiate a counter
+        val counter1 = new Counter()
+
+        // Make sure the first counter works
+        val counts1 = for (i <- 0 until 10) yield counter1.next()
+        assert(counts1 === Vector(1,2,3,4,5,6,7,8,9,10))
+
+        //Instantiate another counter
+        val counter2 = new Counter()
+
+        // Make sure the second one works independently of the first
+        val counts2 = for (i <- 0 until 10) yield counter2.next()
+        assert(counts2 === Vector(1,2,3,4,5,6,7,8,9,10))
+
+        // And the first one has not lost count
+        val counts1_1 = for (i <- 0 until 10) yield counter1.next()
+        assert(counts1_1 === Vector(11,12,13,14,15,16,17,18,19,20))
+    }
+
+    "The hasNext method of a Generator" should "return false when the Generator has been exhausted" in {
+        object CountToThree extends Generator[Int] {
+            def generate() = {
+                produce(1)
+                produce(2)
+                produce(3)
+            }
+        }
+        assert(CountToThree.next() == 1)
+        assert(CountToThree.next() == 2)
+        assert(CountToThree.next() == 3)
+        assert(!CountToThree.hasNext)
+    }
+
+    it should "return true when the Generator has not yet been exhausted" in {
+        object CountToThree extends Generator[Int] {
+            def generate() = {
+                produce(1)
+                produce(2)
+                produce(3)
+            }
+        }
+        assert(CountToThree.hasNext)
+        assert(CountToThree.next() == 1)
+        assert(CountToThree.hasNext)
+        assert(CountToThree.next() == 2)
+        assert(CountToThree.hasNext)
+        assert(CountToThree.next() == 3)
+    }
+
+    "The next method of a Generator" should "throw a NoSuchElementException when the Generator has been exhausted" in {
+        object CountToThree extends Generator[Int] {
+            def generate() = {
+                produce(1)
+                produce(2)
+                produce(3)
+            }
+        }
+        assert(CountToThree.next() == 1)
+        assert(CountToThree.next() == 2)
+        assert(CountToThree.next() == 3)
+        intercept[NoSuchElementException] {
+            CountToThree.next()
+        }
     }
 }
